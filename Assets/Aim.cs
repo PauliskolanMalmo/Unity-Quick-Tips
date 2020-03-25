96e5486a578eca0b1c8f0300a5711f4cc3560213
Paul B <paul.belfrage@gmail.com>
2020-03-25 13:52:05 +0100

    start
    start

---
+++ Assets/Aim.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Aim : MonoBehaviour
+{
+    [SerializeField] Transform cubeTrans;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //direction = destination - source
+        Vector3 directionToAim = cubeTrans.position - transform.position;
+        Debug.DrawRay(transform.position, directionToAim, Color.green);
+
+        //access current location = Quartionian Look Rotaion
+        //transform.rotation = Quaternion.LookRotation(directionToAim);
+
+        Quaternion targetRotation = Quaternion.LookRotation(directionToAim);
+
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime);
+
+
+    }
+}
---
+++ Assets/Cube.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    public GameObject cubePrefab;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Instantiate(cubePrefab, Vector3.zero, Quaternion.Euler(0, 35, 0));
+            }
+        }
+    }
+}
